c48e1bf44f3936309aa99553d7527fdf56ded89f
John Stegman <john@green-leaf-services.com>
2017-04-12 14:57:52 -0400

    Added Delete for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,7 +22,7 @@ class PortfoliosController < ApplicationController
   def show
       @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
@@ -38,5 +38,18 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
+end
 
 end
