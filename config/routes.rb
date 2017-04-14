afa078de74213ff38f92c10b0ef40b36e9b0729f
John Stegman <john@green-leaf-services.com>
2017-04-14 10:44:52 -0400

    Custom Scopes

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
 
 end
